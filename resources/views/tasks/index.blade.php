4ccc8877d8c88bc2dd570237bac71a230d556a6a
J.N <junnamba@icloud.com>
2021-05-05 12:55:13 +0000

    first commit

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,8 +16,9 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
+                    
                 </tr>
                 @endforeach
             </tbody>
